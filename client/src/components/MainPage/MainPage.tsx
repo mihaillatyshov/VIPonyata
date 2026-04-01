e605c917acabfd3e23ef73a6f8fdf9727fb4ad17
mihaillatyshov <mihail.latyshov2@mail.ru>
2026-04-02 00:15:06 +0300

    deleted old link to quizlet

--- client/src/components/MainPage/MainPage.tsx
+++ client/src/components/MainPage/MainPage.tsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 import PageTitle from "components/Common/PageTitle";
 import CoursesList from "components/Courses/CoursesList";
 
@@ -7,11 +5,6 @@ const MainPage = () => {
     return (
         <div className="container">
             <PageTitle title="コース" />
-            <div className="mb-3">
-                <Link to="/quizlet" className="btn btn-outline-primary">
-                    Перейти в Quizlet
-                </Link>
-            </div>
             <CoursesList />
         </div>
     );
