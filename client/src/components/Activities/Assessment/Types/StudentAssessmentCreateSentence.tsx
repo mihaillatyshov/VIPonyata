b0d80b317dfd8bdf2912c71059a0800e45c7e3c4
mihaillatyshov <mihail.latyshov2@mail.ru>
2026-04-04 23:46:45 +0300

    changed task "make a sentence"

--- client/src/components/Activities/Assessment/Types/StudentAssessmentCreateSentence.tsx
+++ client/src/components/Activities/Assessment/Types/StudentAssessmentCreateSentence.tsx
@@ -1,33 +1,126 @@
-import React from "react";
+import { useEffect, useMemo, useState } from "react";
 
+import { FindMaxStr, fixRubyStr } from "libs/Autisize";
 import { TAssessmentCreateSentence } from "models/Activity/Items/TAssessmentItems";
 import { useAppDispatch } from "redux/hooks";
 import { setAssessmentTaskData } from "redux/slices/assessmentSlice";
 
-import { DragEndEvent } from "@dnd-kit/core";
-import { arrayMove } from "@dnd-kit/sortable";
+import {
+    DndContext,
+    DragEndEvent,
+    KeyboardSensor,
+    MouseSensor,
+    TouchSensor,
+    useSensor,
+    useSensors,
+} from "@dnd-kit/core";
 
-import SortableOrder from "./DndSortable/SortableOrder";
+import { isFieldData } from "./StudentAssessmentFillSpacesExists/Draggable";
+import Droppable from "./StudentAssessmentFillSpacesExists/Droppable";
+import InputsField from "./StudentAssessmentFillSpacesExists/InputsField";
 import { StudentAssessmentTypeProps } from "./StudentAssessmentTypeProps";
 
 const StudentAssessmentCreateSentence = ({ data, taskId }: StudentAssessmentTypeProps<TAssessmentCreateSentence>) => {
     const dispatch = useAppDispatch();
+    const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor), useSensor(KeyboardSensor));
+
+    const [answers, setAnswers] = useState<(string | null)[]>(() => Array(data.parts.length).fill(null));
+    const [inputs, setInputs] = useState<string[]>(() => [...data.parts]);
+
+    useEffect(() => {
+        setAnswers(Array(data.parts.length).fill(null));
+        setInputs([...data.parts]);
+    }, [taskId]);
+
+    const syncTaskData = (nextAnswers: (string | null)[], nextInputs: string[]) => {
+        const orderedFilledParts = nextAnswers.filter((item): item is string => item !== null);
+        const nextParts = [...orderedFilledParts, ...nextInputs];
+        dispatch(setAssessmentTaskData({ id: taskId, data: { ...data, parts: nextParts } }));
+    };
+
+    const handleAnsInp = (answerId: number) => {
+        const currentAnswer = answers[answerId];
+        if (currentAnswer === null) {
+            return;
+        }
+
+        const nextAnswers = [...answers];
+        const nextInputs = [...inputs, currentAnswer];
+        nextAnswers[answerId] = null;
+
+        setAnswers(nextAnswers);
+        setInputs(nextInputs);
+        syncTaskData(nextAnswers, nextInputs);
+    };
+
+    const handleInpAns = (inputId: number, answerId: number) => {
+        const nextInputs = [...inputs];
+        const nextAnswers = [...answers];
+
+        const selectedInput = nextInputs.splice(inputId, 1)[0];
+        const currentAnswer = nextAnswers[answerId];
+
+        if (currentAnswer !== null) {
+            nextInputs.push(currentAnswer);
+        }
+        nextAnswers[answerId] = selectedInput;
+
+        setAnswers(nextAnswers);
+        setInputs(nextInputs);
+        syncTaskData(nextAnswers, nextInputs);
+    };
+
+    const handleAnsAns = (fromId: number, toId: number) => {
+        const nextAnswers = [...answers];
+        [nextAnswers[fromId], nextAnswers[toId]] = [nextAnswers[toId], nextAnswers[fromId]];
+
+        setAnswers(nextAnswers);
+        syncTaskData(nextAnswers, inputs);
+    };
 
     const handleDragEnd = (event: DragEndEvent) => {
         const { active, over } = event;
-        if (over === null) return;
+        const activeData = active.data.current;
+        if (!isFieldData(activeData)) {
+            return;
+        }
 
-        if (active?.data?.current?.arrayId !== over?.data?.current?.arrayId) {
-            data.parts = arrayMove(
-                data.parts,
-                active.data.current?.arrayId as number,
-                over.data.current?.arrayId as number,
-            );
-            dispatch(setAssessmentTaskData({ id: taskId, data: data }));
+        if (over === null) {
+            if (activeData.type === "answer") {
+                handleAnsInp(activeData.fieldId);
+            }
+            return;
+        }
+
+        const overData = over.data.current;
+        if (!isFieldData(overData)) {
+            return;
+        }
+
+        if (activeData.type === "inputs" && overData.type === "answer") {
+            handleInpAns(activeData.fieldId, overData.fieldId);
+        } else if (activeData.type === "answer" && overData.type === "answer") {
+            handleAnsAns(activeData.fieldId, overData.fieldId);
         }
     };
 
-    return <SortableOrder handleDragEnd={handleDragEnd} data={data} order="horizontal" />;
+    const longestStr = useMemo(
+        () => FindMaxStr([...inputs, ...answers].filter((item) => item !== null) as string[], fixRubyStr),
+        [answers, inputs],
+    );
+
+    return (
+        <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+            <div className="student-assessment-fill-spaces__container">
+                <InputsField inputFields={inputs} longestStr={longestStr} />
+                <div className="student-assessment-fill-spaces student-assessment-create-sentence__answers">
+                    {answers.map((answer, fieldId) => (
+                        <Droppable key={fieldId} id={fieldId} longestStr={longestStr} str={answer} />
+                    ))}
+                </div>
+            </div>
+        </DndContext>
+    );
 };
 
 export default StudentAssessmentCreateSentence;
